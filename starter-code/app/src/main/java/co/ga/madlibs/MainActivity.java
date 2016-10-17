76997f1bdb4d3a5dca23692aad5aaac7dd18d4c4
ECarrasco91 <ezequielcarrasco91@yahoo.com>
2016-10-17 00:54:24 -0400

    changes made

--- starter-code/app/src/main/java/co/ga/madlibs/MainActivity.java
+++ starter-code/app/src/main/java/co/ga/madlibs/MainActivity.java
@@ -1,12 +1,62 @@
 package co.ga.madlibs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mAdj1;
+    private EditText mAdj2;
+    private EditText mNoun1;
+    private EditText mNoun2;
+    private EditText mAnimal;
+    private EditText mGame;
+    private Button mButton;
+
+    private List<EditText> fields = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAdj1 = (EditText) findViewById(R.id.adj1);
+        mAdj2 = (EditText) findViewById(R.id.adj2);
+        mNoun1 = (EditText) findViewById(R.id.noun1);
+        mNoun2 = (EditText) findViewById(R.id.noun2);
+        mAnimal = (EditText) findViewById(R.id.animal);
+        mGame  = (EditText) findViewById(R.id.game);
+        mButton = (Button) findViewById(R.id.button);
+
+        fields.add(mAdj1);
+        fields.add(mAdj2);
+        fields.add(mNoun1);
+        fields.add(mNoun2);
+        fields.add(mAnimal);
+        fields.add(mGame);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resultAct = new Intent(MainActivity.this, ResultActivity.class);
+                boolean cont = true;
+                for (EditText field: fields ) {
+                    String str = field.getText().toString();
+                    if (str.equals("")){
+                        field.setError("Fill field!");
+                        cont = false;
+                    } else if (cont){
+                        resultAct.putExtra("input", str);
+                        startActivity(resultAct);
+                    }
+                }
+            }
+        });
     }
 }
--- starter-code/app/src/main/java/co/ga/madlibs/ResultActivity.java
+++ starter-code/app/src/main/java/co/ga/madlibs/ResultActivity.java
@@ -1,5 +1,6 @@
 package co.ga.madlibs;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
@@ -9,10 +10,17 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.List;
+
 /**
  * Created by james on 12/7/15.
  */
 public class ResultActivity extends AppCompatActivity {
+    private final String text = "A vacation is when you take a trip to some" +
+            " %s place with your %s family. Usually, you go to some place" +
+            " that is near a %s or up on a %s. A good vacation place is one" +
+            " where you can ride %s or play %s";
+    private TextView mText;
     private Button mBackButton;
 
     @Override
@@ -20,7 +28,13 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
+        mText = (TextView) findViewById(R.id.result_textview);
         mBackButton = (Button) findViewById(R.id.back_button);
+
+        Intent passed = getIntent();
+        List<String> fields = passed.getStringArrayListExtra("input");
+
+        mText.setText(String.format(text, fields));
 
         mBackButton.setOnClickListener(new View.OnClickListener() {
             @Override
